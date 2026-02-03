028539b68154cae42eaacc33ab3b71decfabf58c
acevif <acevif@gmail.com>
2026-02-03 19:33:05 +0900

    Refine help header text

--- Sources/KenshinCheckupCLI/HelpSubcommand.swift
+++ Sources/KenshinCheckupCLI/HelpSubcommand.swift
@@ -8,32 +8,32 @@ public struct HelpSubcommand: ParsableCommand {
 
     public init() {}
 
-    public static func usageLines() -> [String] {
-        [
-            "kenshin - config health checks (current: chezmoi-unmanaged)",
-            "",
-            "Usage:",
-            "  kenshin",
-            "  kenshin help",
-            "  kenshin checkup",
-            "  kenshin version",
-            "  kenshin --help",
-            "  kenshin -h",
-            "  kenshin --version",
-            "  kenshin -v",
-            "  kenshin --config <path>",
-            "  kenshin -c <path>",
-            "  kenshin checkup --config <path>",
-            "  kenshin checkup -c <path>",
-            "",
-            "Config:",
-            "  ~/.config/kenshin/config.toml",
-        ]
+    public static func usageText() -> String {
+        """
+        KenshinCheckup (kenshin) performs multiple doctor checks in one go.
+
+        Repository: https://github.com/acevif/kenshincheckup
+
+        Usage:
+          kenshin
+          kenshin help
+          kenshin checkup
+          kenshin version
+          kenshin --help
+          kenshin -h
+          kenshin --version
+          kenshin -v
+          kenshin --config <path>
+          kenshin -c <path>
+          kenshin checkup --config <path>
+          kenshin checkup -c <path>
+
+        Config:
+          ~/.config/kenshin/config.toml
+        """
     }
 
     public func run() throws {
-        for line in Self.usageLines() {
-            print(line)
-        }
+        print(Self.usageText())
     }
 }
