21a8416df8e2f9c576195f25921c3450e8653667
acevif <acevif@gmail.com>
2026-02-03 19:13:42 +0900

    Move usage lines to HelpSubcommand

--- Sources/KenshinCheckupCLI/HelpSubcommand.swift
+++ Sources/KenshinCheckupCLI/HelpSubcommand.swift
@@ -8,8 +8,31 @@ public struct HelpSubcommand: ParsableCommand {
 
     public init() {}
 
+    public static func usageLines() -> [String] {
+        [
+            "kenshin - config health checks (current: chezmoi-unmanaged)",
+            "",
+            "Usage:",
+            "  kenshin",
+            "  kenshin help",
+            "  kenshin checkup",
+            "  kenshin version",
+            "  kenshin --help",
+            "  kenshin -h",
+            "  kenshin --version",
+            "  kenshin -v",
+            "  kenshin --config <path>",
+            "  kenshin -c <path>",
+            "  kenshin checkup --config <path>",
+            "  kenshin checkup -c <path>",
+            "",
+            "Config:",
+            "  ~/.config/kenshin/config.toml",
+        ]
+    }
+
     public func run() throws {
-        for line in KenshinCheckupCommand.usageLines() {
+        for line in Self.usageLines() {
             print(line)
         }
     }
--- Sources/KenshinCheckupCLI/KenshinCheckupCommand.swift
+++ Sources/KenshinCheckupCLI/KenshinCheckupCommand.swift
@@ -17,29 +17,6 @@ public struct KenshinCheckupCommand: ParsableCommand {
 
     public init() {}
 
-    public static func usageLines() -> [String] {
-        [
-            "kenshin - config health checks (current: chezmoi-unmanaged)",
-            "",
-            "Usage:",
-            "  kenshin",
-            "  kenshin help",
-            "  kenshin checkup",
-            "  kenshin version",
-            "  kenshin --help",
-            "  kenshin -h",
-            "  kenshin --version",
-            "  kenshin -v",
-            "  kenshin --config <path>",
-            "  kenshin -c <path>",
-            "  kenshin checkup --config <path>",
-            "  kenshin checkup -c <path>",
-            "",
-            "Config:",
-            "  ~/.config/kenshin/config.toml",
-        ]
-    }
-
     public func run() throws {
         if version {
             print(VersionSubcommand.versionOutput())
