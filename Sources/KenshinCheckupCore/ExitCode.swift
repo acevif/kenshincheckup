601ec653ad8864754884766ead38ca6f9f1398be
acevif <acevif@gmail.com>
2026-02-04 18:25:09 +0900

    Refine ExitCode doc comment

--- Sources/KenshinCheckupCore/ExitCode.swift
+++ Sources/KenshinCheckupCore/ExitCode.swift
@@ -2,11 +2,8 @@ import Tagged
 
 /// Represents a process termination status (exit code).
 ///
-/// Use this type only for exit codes returned by running external processes,
-/// not for internal app logic or domain results.
-///
-/// To terminate this CLI with a specific exit code, throw
-/// `ArgumentParser.ExitCode` from a `ParsableCommand`.
+/// This type is for exit codes observed from external processes.
+/// It should not be used to model the app’s own check outcomes or status.
 public typealias ExitCode = Tagged<ExitCodeTag, Int32>
 
 public enum ExitCodeTag {}
